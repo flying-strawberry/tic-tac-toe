3916dd3ea59e9d808527ea2cb65b8020f25e8fb0
flying-strawberry <eitri@disroot.org>
2022-06-21 05:26:35 -0700

    Create print board function

--- main.cpp
+++ main.cpp
@@ -8,28 +8,44 @@ array<char, 9> board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 int coord_to_index(int, int);
 void print_board(string, string);
 
-int main() {
+int main()
+{
     int noOfPlayers;
     string player1;
     string player2;
-    cout<<"  Welcome to the Tic Tac Toe  \n";
-    cout<<"Enter the Numbers of player(1/2)";
-    cin>>noOfPlayers;
-    cout<<"Enter player 1 name: \n";
-    cin>>player1;
-    cout<<"Enter player 2 name: \n";
-    cin>>player2;
-    
-    
+    cout << "  Welcome to the Tic Tac Toe  \n";
+    cout << "Enter the Numbers of player(1/2)";
+    cin >> noOfPlayers;
+    cout << "Enter player 1 name: \n";
+    cin >> player1;
+    cout << "Enter player 2 name: \n";
+    cin >> player2;
 
     return 0;
 }
 
-int coord_to_index(int x, int y) {
-    if (x > 3 || y > 3) {
+int coord_to_index(int x, int y)
+{
+    if (x > 3 || y > 3)
+    {
         return -1;
-    } else {
+    }
+    else
+    {
         y--, x--;
         return 3 * y + x;
     }
+}
+
+void print_board(string player1, string player2) {
+    cout << '\n' << "Player 1: " << player1 << '\n';
+    cout << "_________\n";
+    int j = 0;
+    for (int i = 0; i < 3; i++) {
+        cout << "|   |   |   |\n";
+        cout << "| " << board[j] << " | " << board[j + 1] << " | " << board[j + 2] << " |";
+        cout << "|   |   |   |\n";
+        j += 3;
+    }
+    cout << '\n' << "Player 2: " << player2 << '\n';
 }
