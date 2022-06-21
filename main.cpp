8c914da153a840f1b183bbf4332ae948412840f6
flying-strawberry <eitri@disroot.org>
2022-06-21 06:45:54 -0700

    Final for two players

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ array<char, 9> board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 int coord_to_index(int, int);
 void print_board(string, string);
 bool is_filled(int);
+bool check_win(int, int);
 
 int main()
 {
@@ -49,6 +50,19 @@ int main()
             continue;
         }
         board[index] = token;
+        if (check_win(x, y))
+        {
+            print_board(player1, player2);
+            if (token == 'X')
+            {
+                cout << player1 << " won!\n";
+            }
+            else
+            {
+                cout << player2 << " won!\n";
+            }
+            break;
+        }
         if (token == 'X')
         {
             token = 'O';
@@ -72,6 +86,27 @@ int coord_to_index(int x, int y)
         y--, x--;
         return 3 * y + x;
     }
+}
+
+bool check_win(int x, int y)
+{
+    if ( (board[coord_to_index(x, 1)] == board[coord_to_index(x, 2)]) && (board[coord_to_index(x, 2)] == board[coord_to_index(x, 3)]) ) {
+        return true;
+    }
+    else if ( (board[coord_to_index(1, y)] == board[coord_to_index(2, y)]) && (board[coord_to_index(2, y)] == board[coord_to_index(3, y)]) ) {
+        return true;
+    }
+    else if ( (board[coord_to_index(1, 1)] == board[coord_to_index(2, 2)]) && (board[coord_to_index(2, 2)] == board[coord_to_index(3, 3)]) ) {
+        if ( board[coord_to_index(1, 1)] != ' ' && board[coord_to_index(2, 2)] != ' ' && board[coord_to_index(3, 3)] != ' ') {
+            return true;
+        }
+    }
+    else if ( (board[coord_to_index(3, 1)] == board[coord_to_index(2, 2)]) && (board[coord_to_index(2, 2)] == board[coord_to_index(1, 3)]) ) {
+        if (board[coord_to_index(3, 1)] != ' ' && board[coord_to_index(2, 2)] != ' ' && board[coord_to_index(1, 3)] != ' ') {
+            return true;
+        }
+    }
+    return false;
 }
 
 void print_board(string player1, string player2)
