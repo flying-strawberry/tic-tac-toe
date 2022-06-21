2c84af67109f163e38f5ee70bed98a2a70b868d7
flying-strawberry <eitri@disroot.org>
2022-06-21 04:45:42 -0700

    Create a function to coordinates to array index for move input

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 array<char, 9> board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
+int coord_to_index(int, int);
+
 int main() {
     cout << "Hello, world.\n";
     return 0;
+}
+
+int coord_to_index(int x, int y) {
+    if (x > 3 || y > 3) {
+        return -1;
+    } else {
+        y--, x--;
+        return 3 * y + x;
+    }
 }
