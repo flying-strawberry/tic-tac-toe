69b4dc389de252acd782d3cee8fc3be24aeaa37e
Zaid and Maarif <mdzaid786291@gmail.com>
2022-06-21 17:33:12 +0530

    Menu

--- main.cpp
+++ main.cpp
@@ -8,7 +8,18 @@ array<char, 9> board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 int coord_to_index(int, int);
 
 int main() {
-    cout << "Welcome to \n";
+    int noOfPlayers;
+    string player1;
+    string player2;
+    cout<<"  Welcome to the Tic Tac Toe  \n";
+    cout<<"Enter the Numbers of player(1/2)";
+    cin>>noOfPlayers;
+    cout<<"Enter player 1 name: \n";
+    cin>>player1;
+    cout<<"Enter player 2 name: \n";
+    cin>>player2;
+    
+    
 
     return 0;
 }
