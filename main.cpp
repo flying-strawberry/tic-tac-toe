7e07705f53f6b9dda64c458ad1cdb7400a6829e1
Zaid and Maarif <mdzaid786291@gmail.com>
2022-06-21 17:49:56 +0530

    Taking players choice

--- main.cpp
+++ main.cpp
@@ -8,27 +8,48 @@ array<char, 9> board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 int coord_to_index(int, int);
 void print_board();
 
-int main() {
+int main()
+{
     int noOfPlayers;
     string player1;
     string player2;
-    cout<<"  Welcome to the Tic Tac Toe  \n";
-    cout<<"Enter the Numbers of player(1/2)";
-    cin>>noOfPlayers;
-    cout<<"Enter player 1 name: \n";
-    cin>>player1;
-    cout<<"Enter player 2 name: \n";
-    cin>>player2;
-    
-    
+    cout << "  Welcome to the Tic Tac Toe  \n";
+    cout << "Enter the Numbers of player(1/2)";
+    cin >> noOfPlayers;
+    cout << "Enter player 1 name: \n";
+    cin >> player1;
+    cout << "Enter player 2 name: \n";
+    cin >> player2;
+
+    print_board()
+    {
+        int choice;
+        char token = 'x';
+
+        if(token=='x'){
+            cout<<player1<<" enter your choice: ";
+            cin>>choice;
+        }
+        if(token == 'y'){
+            cout<<player2<<" enter your choice: ";
+            cin>>choice;
+        }
+
+        
+
+    }
 
     return 0;
 }
 
-int coord_to_index(int x, int y) {
-    if (x > 3 || y > 3) {
+int coord_to_index(int x, int y)
+{
+    if (x > 3 || y > 3)
+    {
         return -1;
-    } else {
+    }
+    else
+    {
         y--, x--;
         return 3 * y + x;
     }
